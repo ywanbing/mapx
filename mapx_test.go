ed4a742c19bf1e01b8fc7a89399af936f90215dd
ywb <yangwanbing@xingqiwan.com>
2022-07-08 11:08:20 +0800

    增加结构体的测试

--- mapx_test.go
+++ mapx_test.go
@@ -3,8 +3,16 @@ package mapx
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"testing"
 )
+
+type MapTestS struct {
+	id   int
+	id2  int32
+	str  string
+	str2 string
+}
 
 func TestNewMapx(t *testing.T) {
 	_ = NewMapx[int32, int32](16)
@@ -15,12 +23,32 @@ func TestNewMapx(t *testing.T) {
 	_ = NewMapx[string, []byte](16)
 	_ = NewMapx[*int, string](16)
 	_ = NewMapx[*int32, *int32](16)
+	_ = NewMapx[int, *MapTestS](16)
+	_ = NewMapx[int, MapTestS](16)
 }
 
 func TestMapx_Get(t *testing.T) {
 	m := NewMapx[int, int32](16)
 	for i := 0; i < 16; i++ {
 		m.Set(int(i), int32(i))
+	}
+
+	for i := 0; i < 50; i++ {
+		n := rand.Int31n(16)
+		v := m.Get(int(n))
+		fmt.Println(v)
+	}
+}
+
+func TestMapx_GetStruct(t *testing.T) {
+	m := NewMapx[int, MapTestS](16)
+	for i := 0; i < 16; i++ {
+		m.Set(int(i), MapTestS{
+			id:   int(rand.Int31n(16)),
+			id2:  rand.Int31n(16),
+			str:  strconv.Itoa(int(rand.Int31n(16))),
+			str2: strconv.Itoa(int(rand.Int31n(16))),
+		})
 	}
 
 	for i := 0; i < 50; i++ {
@@ -104,6 +132,172 @@ func Benchmark_Get20(b *testing.B) {
 	for i := 0; i < 20; i++ {
 		mx.Set(int(i), int32(i))
 		m[i] = int32(i)
+	}
+
+	b.Run("Mapx", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			mx.Get(i % 20)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			i2 := m[i%20]
+			_ = i2
+		}
+	})
+}
+
+func Benchmark_GetStruct8(b *testing.B) {
+	mx := NewMapx[int, MapTestS](8)
+	m := make(map[int]MapTestS, 8)
+	for i := 0; i < 8; i++ {
+		v := MapTestS{
+			id:   int(rand.Int31n(100)),
+			id2:  rand.Int31n(100),
+			str:  strconv.Itoa(int(rand.Int31n(100))),
+			str2: strconv.Itoa(int(rand.Int31n(100))),
+		}
+		mx.Set(int(i), v)
+		m[i] = v
+	}
+
+	b.Run("Mapx", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			mx.Get(i % 8)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			i2 := m[i%8]
+			_ = i2
+		}
+	})
+}
+
+func Benchmark_GetStruct16(b *testing.B) {
+	mx := NewMapx[int, MapTestS](16)
+	m := make(map[int]MapTestS, 16)
+	for i := 0; i < 16; i++ {
+		v := MapTestS{
+			id:   int(rand.Int31n(100)),
+			id2:  rand.Int31n(100),
+			str:  strconv.Itoa(int(rand.Int31n(100))),
+			str2: strconv.Itoa(int(rand.Int31n(100))),
+		}
+		mx.Set(int(i), v)
+		m[i] = v
+	}
+	b.Run("Mapx", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			mx.Get(i % 16)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			i2 := m[i%16]
+			_ = i2
+		}
+	})
+}
+
+func Benchmark_GetStruct20(b *testing.B) {
+	mx := NewMapx[int, MapTestS](20)
+	m := make(map[int]MapTestS, 20)
+	for i := 0; i < 20; i++ {
+		v := MapTestS{
+			id:   int(rand.Int31n(100)),
+			id2:  rand.Int31n(100),
+			str:  strconv.Itoa(int(rand.Int31n(100))),
+			str2: strconv.Itoa(int(rand.Int31n(100))),
+		}
+		mx.Set(int(i), v)
+		m[i] = v
+	}
+
+	b.Run("Mapx", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			mx.Get(i % 20)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			i2 := m[i%20]
+			_ = i2
+		}
+	})
+}
+
+func Benchmark_GetStructPtr8(b *testing.B) {
+	mx := NewMapx[int, *MapTestS](8)
+	m := make(map[int]*MapTestS, 8)
+	for i := 0; i < 8; i++ {
+		v := MapTestS{
+			id:   int(rand.Int31n(100)),
+			id2:  rand.Int31n(100),
+			str:  strconv.Itoa(int(rand.Int31n(100))),
+			str2: strconv.Itoa(int(rand.Int31n(100))),
+		}
+		mx.Set(int(i), &v)
+		m[i] = &v
+	}
+
+	b.Run("Mapx", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			mx.Get(i % 8)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			i2 := m[i%8]
+			_ = i2
+		}
+	})
+}
+
+func Benchmark_GetStructPtr16(b *testing.B) {
+	mx := NewMapx[int, *MapTestS](16)
+	m := make(map[int]*MapTestS, 16)
+	for i := 0; i < 16; i++ {
+		v := MapTestS{
+			id:   int(rand.Int31n(100)),
+			id2:  rand.Int31n(100),
+			str:  strconv.Itoa(int(rand.Int31n(100))),
+			str2: strconv.Itoa(int(rand.Int31n(100))),
+		}
+		mx.Set(int(i), &v)
+		m[i] = &v
+	}
+	b.Run("Mapx", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			mx.Get(i % 16)
+		}
+	})
+
+	b.Run("map", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			i2 := m[i%16]
+			_ = i2
+		}
+	})
+}
+
+func Benchmark_GetStructPtr20(b *testing.B) {
+	mx := NewMapx[int, *MapTestS](20)
+	m := make(map[int]*MapTestS, 20)
+	for i := 0; i < 20; i++ {
+		v := MapTestS{
+			id:   int(rand.Int31n(100)),
+			id2:  rand.Int31n(100),
+			str:  strconv.Itoa(int(rand.Int31n(100))),
+			str2: strconv.Itoa(int(rand.Int31n(100))),
+		}
+		mx.Set(int(i), &v)
+		m[i] = &v
 	}
 
 	b.Run("Mapx", func(b *testing.B) {
